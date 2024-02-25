1d0afbb75dfa3bae8f7d8d323728ae50051f4f7c
joaaobr <jlaod102@gmail.com>
2024-02-25 17:19:47 -0300

    remove unused imports

--- src/main/scala/vsm/document/DocumentFile.scala
+++ src/main/scala/vsm/document/DocumentFile.scala
@@ -1,13 +1,6 @@
 package com.searcher.vsm.document
 
-import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.Future
-import scala.io.BufferedSource
-import java.io.BufferedReader
-import java.io.FileReader
-import java.io.*;
 import java.nio.file.*;
-import java.util.stream.*;
 
 class DocumentFile(file: String) {
   def read(): Seq[String] = {
