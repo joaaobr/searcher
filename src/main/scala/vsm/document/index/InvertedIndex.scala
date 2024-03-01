46875d940697bdd66c1c08416eb4ac8da4cd7ff1
joaaobr <jlaod102@gmail.com>
2024-03-01 15:38:37 -0300

    rename variable

--- src/main/scala/vsm/document/index/InvertedIndex.scala
+++ src/main/scala/vsm/document/index/InvertedIndex.scala
@@ -2,27 +2,30 @@ package com.searcher.vsm.document.index
 
 import scala.collection.mutable.HashMap
 
+/*
+    This module is used to index phrases and to facilitate the search and obtain the closest documents.
+*/
+
 class InvertedIndex {
-    val table: HashMap[String, Set[Int]] = new HashMap[String, Set[Int]]
+    val index: HashMap[String, Set[Int]] = new HashMap[String, Set[Int]]
 
     def get(key: String): Set[Int] = {
-        table.get(key).get
+        index.get(key).get
     }
 
     def insertSeq(keys: Seq[String], value: Int) = {
         keys
         .map(key => update(key, value))
-        
     }
 
     def update(key: String, id: Int) = {
-        val ids = table.getOrElseUpdate(key, Set()) ++ Set(id)
-        table.update(key, ids)
+        val ids = index.getOrElseUpdate(key, Set()) ++ Set(id)
+        index.update(key, ids)
     }
 
     def getKeys(keys: Seq[String]) = {
         keys
-        .map(key => table.getOrElseUpdate(key, Set()))
+        .map(key => index.getOrElseUpdate(key, Set()))
         .reduce((a, b) => a ++ b)
         .toSet
     }
