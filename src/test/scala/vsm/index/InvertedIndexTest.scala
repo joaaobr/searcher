c74f2ab592dea3e80fe56aba5a05fe5f57e9d9da
joaaobr <jlaod102@gmail.com>
2024-03-05 20:31:45 -0300

    added: tests for inverted index

---
+++ src/test/scala/vsm/index/InvertedIndexTest.scala
@@ -0,0 +1,81 @@
+import com.searcher.vsm.document.index.InvertedIndex
+
+class InvertedIndexTest extends munit.FunSuite {
+  test("Insersion and get value") {
+    val indexer = new InvertedIndex()
+    val preset = Seq("key")
+
+    indexer.insertSeq(preset, 5)
+
+    val result = indexer.get("key")
+    val expected = Set(5)
+
+    assert(result == expected)
+  }
+
+  test("Find empty value") {
+    val indexer = new InvertedIndex()
+
+    val result = indexer.get("empty")
+    val expected = Set()
+
+    assert(result == expected)
+  }
+
+  test("Find value with multiples ids") {
+    val indexer = new InvertedIndex()
+    val preset = Seq("key")
+
+    indexer.insertSeq(preset, 5)
+    indexer.insertSeq(preset, 6)
+    indexer.insertSeq(preset, 7)
+
+    val result = indexer.get("key")
+    val expected = Set(5, 6, 7)
+
+    assert(result == expected)
+  }
+
+  test("Find value with multiples ids") {
+    val indexer = new InvertedIndex()
+    val preset = Seq("key")
+    val preset2 = Seq("word")
+
+    indexer.insertSeq(preset, 5)
+    indexer.insertSeq(preset, 6)
+    indexer.insertSeq(preset, 7)
+
+    indexer.insertSeq(preset2, 8)
+    indexer.insertSeq(preset2, 9)
+    indexer.insertSeq(preset2, 10)
+
+    val result = indexer.getIds(Seq("key", "word"))
+    val expected = Set(5, 6, 7, 8, 9, 10)
+
+    assert(result == expected)
+  }
+
+  test("Get empty value") {
+    val indexer = new InvertedIndex()
+
+    val result = indexer.getIds(Seq("empty"))
+    val expected = Set()
+
+    assert(result == expected)
+  }
+
+  test("Update value") {
+    val indexer = new InvertedIndex()
+    val preset = Seq("key")
+
+    indexer.insertSeq(preset, 5)
+    indexer.insertSeq(preset, 6)
+    indexer.update("key", 7)
+
+    val result = indexer.getIds(Seq("key"))
+    val expected = Set(5, 6, 7)
+
+    assert(result == expected)
+  }
+
+}
